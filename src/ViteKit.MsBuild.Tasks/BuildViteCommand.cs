f6da5754bb4adbaec4ce04752838b1e84e148d64
Chris McGrath <chris.mcgrath@redcross.ca>
2025-11-24 02:39:53 -0500

    refactor: clean up BuildViteCommand.cs and update project metadata in ViteKit.MsBuild.csproj

--- src/ViteKit.MsBuild.Tasks/BuildViteCommand.cs
+++ src/ViteKit.MsBuild.Tasks/BuildViteCommand.cs
@@ -1,9 +1,7 @@
 using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Text;
 using System.Threading;
     
